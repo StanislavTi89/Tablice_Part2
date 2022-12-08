6b1589dcf733be9e5a7fd58d221c4586ccf79fca
StanislavTi89 <Timoshenko311989@gmail.com>
2022-12-08 21:20:43 +0300

    Start_Tablice_Part2 (Массивы_Часть_2)

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
@@ -29,6 +30,11 @@ public class Main {
         //Задание 4
         System.out.println("Задание 4");
         String string = "Иванов Иван Иванович";
+        char[] stringToChar = string.toCharArray();
+        System.out.println(Arrays.toString(stringToChar));
+        for (int i = stringToChar.length - 1; i>=0; i--){
+            System.out.print(stringToChar[i]);
+        }
 
     }
 
