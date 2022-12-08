e9b3c1a8c0411e31adc5776c9bf60c9ee4658519
StanislavTi89 <Timoshenko311989@gmail.com>
2022-12-08 21:17:12 +0300

    Start_Tablice_Part2 (Массивы_Часть_2)

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,41 @@
-import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
     public static void main(String[ ] args) {
         System.out.println("Массивы_Часть_2");
         System.out.println();
-        int[] arr = generateRandomArray();
+        int[] arr = arrRandom();
         int sum = 0;
         for (int number : arr){
             sum += number;
         }
         System.out.println(sum);
+
+        //Задание 2
+        System.out.println("Задание 2");
+        int min = arr[0];
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++){
+            if (arr[i]<min){
+                min = arr[i];
+            }
+        }
+        System.out.println("Минимальная сумма трат за день составила " + min + " рублей. Максимальная сумма трат за день составила " + max + " рублей");
+
+        //Задание 3
+        System.out.println("Задание 3");
+        System.out.println("Средний расход за месяц, составил: " + (double)sum/arr.length);
+
+        //Задание 4
+        System.out.println("Задание 4");
+        String string = "Иванов Иван Иванович";
+
     }
 
-    public static int[] generateRandomArray() {
+    public static int[] arrRandom() {
+
+        //Задание 1
+        System.out.println("Задание 1");
         Random random = new java.util.Random();
         int[] arr = new int[30];
         for (int i = 0; i < arr.length; i++) {
