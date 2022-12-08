36744371d2047bbe7114bf2aa515ac148c5b522f
StanislavTi89 <Timoshenko311989@gmail.com>
2022-12-08 16:20:50 +0300

    Start_Tablice_Part2 (Массивы_Часть_2)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Массивы_Часть_2!");
+    public static void main(String[ ] args) {
+        int[] arr = generateRandomArray();
+    }
+
+    public static int[] generateRandomArray() {
+        Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
     }
 }
